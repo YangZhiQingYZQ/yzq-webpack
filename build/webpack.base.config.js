f7a59e17883c09589dd3e5b71635ae9abf5a24b6
yangzhiqing <493583130@qq.com>
2019-08-05 02:41:53 +0800

    开发租赁申请页面

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,15 +1,34 @@
-const path = require("path")
-function getEntry(){
-	
+const path = require('path'),
+	HtmlWebpackPlugin = require('html-webpack-plugin') //html插件
+	// CleanWebpackPlugin = require('clean-webpack-plugin') //清除dist文件插件
+
+function getEntry() {
+
 }
 
-console.log(__dirname)
 module.exports = {
-	entry : {
-		app : path.resolve(__dirname,"../src/js/pages/app.js")
+	// 打包入口
+	entry: {
+		app: path.resolve(__dirname, '../src/js/pages/app.js')
 	},
-	output : {
-		path : path.resolve(__dirname,"../dist"),
-		filename:"bundle.js"
-	}
+	// 打包出口
+	output: {
+		path: path.resolve(__dirname, '../dist'),
+		filename: '[name].js'
+	},
+	// 映射设置
+	devtool:'inline-source-map',
+	//设置本地web服务器
+	devServer:{
+		contentBase:"./dist"
+	},
+	// 插件配置
+	plugins: [
+		// new CleanWebpackPlugin({
+		// 	cleanOnceBeforeBuildPatterns : ["../dist"]
+		// }),//清除dist文件夹
+		new HtmlWebpackPlugin({
+			title: "out"
+		})
+	]
 }
--- src/js/pages/app.js
+++ src/js/pages/app.js
@@ -1,1 +1,3 @@
 console.log("aa")
+console.log(__dirname)
+console.error("ajlkad")
